48fff545ad29382e5e9124e4d71e72b3309683b6
zst <zst@localhost>
2024-09-27 14:12:27 +0300

    style

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::io;
 
-
 fn insertion_sort(numbers: &mut Vec<i32>) {
     if numbers.len() < 2 {
         return;
@@ -9,10 +8,12 @@ fn insertion_sort(numbers: &mut Vec<i32>) {
     let mut i = 1;
     loop {
         let mut j = i - 1;
-        while numbers[j] > numbers[j + 1] 
-        {
+        while numbers[j] > numbers[j + 1] {
             numbers.swap(j, j + 1);
-            if j == 0 { break;}
+
+            if j == 0 {
+                break;
+            }
             j = j - 1;
         }
         i += 1;
@@ -27,7 +28,7 @@ fn print_vector(numbers: &Vec<i32>) {
     if numbers.len() < 2 {
         return;
     }
-    
+
     let mut i = 1;
     loop {
         print!(" {}", numbers[i]);
@@ -40,11 +41,12 @@ fn print_vector(numbers: &Vec<i32>) {
 fn main() {
     let mut input = String::new();
     let _ = io::stdin().read_line(&mut input);
-    let mut numbers: Vec<i32> = input.trim().split(' ')
-        .map(|x| x.trim().parse()
-          .expect("Not an integer")).collect();
+    let mut numbers: Vec<i32> = input
+        .trim()
+        .split(' ')
+        .map(|x| x.trim().parse().expect("Not an integer"))
+        .collect();
 
     insertion_sort(&mut numbers);
     print_vector(&numbers);
-
 }
